2ccc021f543e5cd50136034fd007a85ae7c4fba4
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2020-03-12 23:22:51 +0530

    initial commit with basic functionality

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import Infobar from '../InfoBar/Infobar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+
+import './Chat.css';
+
+
+let socket;
+
+const Chat = ({location})=> {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'http://localhost:5000/';
+
+    useEffect(() => {
+       const {name,room} = queryString.parse(location.search);
+        
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', {name, room},(error)=>{
+            if(error){
+                alert(error);
+            }
+        });
+        
+    },[ENDPOINT, location.search]);
+
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            setMessages([...messages, message]);
+        })
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+            console.log("users in room: "+users.name);
+          })
+
+        return()=>{
+            socket.emit('disconnect');
+            socket.off();
+        }
+    },[messages]);
+
+    // function for sending messages
+    const sendMessage = (event)=>{
+        event.preventDefault();
+        console.log(event)
+        if(message){
+            socket.emit('sendMessage', message, ()=> setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+                <Infobar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input 
+                    message={message} 
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
